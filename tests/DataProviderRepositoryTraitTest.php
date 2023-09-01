f71cb1e97231501531b740440eed01a0dd7ab336
Alexander Schranz <alexander@sulu.io>
2023-09-01 03:17:58 +0200

    Use sulu DataProviderRepositoryTrait

--- tests/DataProviderRepositoryTraitTest.php
+++ tests/DataProviderRepositoryTraitTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use App\DataProviderRepositoryTrait;
 use PHPUnit\Framework\TestCase;
+use Sulu\Component\SmartContent\Orm\DataProviderRepositoryTrait;
 
 class DataProviderRepositoryTraitTest extends TestCase
 {
